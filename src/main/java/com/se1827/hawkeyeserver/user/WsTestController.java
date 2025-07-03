3d5035dc20237aa935dbf3fc5bc84c876c835e90
DQ-Bao <doanqbao2506@gmail.com>
2025-07-04 00:58:29 +0700

    fix test ws controller

--- src/main/java/com/se1827/hawkeyeserver/user/WsTestController.java
+++ src/main/java/com/se1827/hawkeyeserver/user/WsTestController.java
@@ -1,6 +1,7 @@
 package com.se1827.hawkeyeserver.user;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Controller;
 public class WsTestController {
     @MessageMapping("/ping")
     @SendTo("/pong")
-    public String onPing() throws Exception {
-        return "pong";
+    public String onPing(@Payload String ping) throws Exception {
+        return ping;
     }
 }
